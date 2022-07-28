5d5d4a34caecd1ef3cf3a8551519889a0d33b21f
Kiriar21 <108755844+Kiriar21@users.noreply.github.com>
2022-07-28 21:11:04 +0200

    Update exportScores.php

--- exportScores.php
+++ exportScores.php
@@ -4,7 +4,7 @@ include("connect.php");
 ?>
 
     <?php
-        $ask='SELECT dane.nr_zawodnika, dane.imie, dane.nazwisko, dane.wiek, dane.kategoria, dane.klasyfikacja, dane.tel, '.$_COOKIE['nmMeta'].'wynik.wynik FROM dane JOIN '.$_COOKIE['nmMeta'].'wynik ON dane.nr_chip='.$_COOKIE['nmMeta'].'wynik.nr_chip GROUP BY '.$_COOKIE['nmMeta'].'wynik.nr_chip ORDER BY '.$_COOKIE['nmMeta'].'wynik.wynik INTO OUTFILE "C:\\Users\\\Kiriar\\\Downloads\\\\'.$_COOKIE['nmMeta'].'wynik.csv" CHARACTER SET "cp1250" FIELDS TERMINATED BY "," ';
+        $ask='SELECT dane.nr_zawodnika, dane.imie, dane.nazwisko, dane.wiek, dane.kategoria, dane.klasyfikacja, dane.tel, '.$_COOKIE['nmMeta'].'wynik.wynik FROM dane JOIN '.$_COOKIE['nmMeta'].'wynik ON dane.nr_chip='.$_COOKIE['nmMeta'].'wynik.nr_chip GROUP BY '.$_COOKIE['nmMeta'].'wynik.nr_chip ORDER BY '.$_COOKIE['nmMeta'].'wynik.wynik INTO OUTFILE "C:/Users/puste/Downloads/'.$_COOKIE['nmMeta'].'wynik.csv" CHARACTER SET "cp1250" FIELDS TERMINATED BY "," ';
         if($result=mysqli_query($conn1,$ask)){
             echo("Pomyślnie eksportowano baze danych z wynikami.");
         }
